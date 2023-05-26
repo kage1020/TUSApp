ff75a20e5b790e90c52098dde98461950597cc1b
tus0inoue1007 <4620007@ed.tus.ac.jp>
2023-05-26 15:56:18 +0900

    変更

--- src/app/pages/page.js
+++ src/app/pages/page.js
@@ -16,7 +16,7 @@ const App = () => {
             <Link href='/pages/todo'>ToDoList</Link>
           </li>
           <li>
-            <Link href='/posts'>Member_status</Link>
+            <Link href='/posts/status'>Member_status</Link>
           </li>
         </ul>
       </div>
