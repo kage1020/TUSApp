4bc54ef4eb568ef92c6304b546f6b63c9dece6e9
tus0inoue1007 <4620007@ed.tus.ac.jp>
2023-05-26 15:41:53 +0900

    えh

--- src/app/app.tsx
+++
@@ -1,9 +0,0 @@
-import Page_status from './pages/Page_status'
-import Page_todo from './pages/Page_todo'
-import Layout from './pages/home'
-
-const App: React.FC = () => {
-  return <Layout />
-}
-
-export default App
---
+++ src/app/pages/page.js
@@ -0,0 +1,9 @@
+import Page_status from './Page_status'
+import Page_todo from './Page_todo'
+import Layout from './home'
+
+const App = () => {
+  return <Layout />
+}
+
+export default App
