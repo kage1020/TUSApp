bbe4bef9769999ef56d74be83014df9e27a14274
tus0inoue1007 <4620007@ed.tus.ac.jp>
2023-05-30 13:37:26 +0900

    inoue

--- src/app/pages/copy/index_cp.ts
+++ src/app/pages/copy/index_cp.ts
@@ -1,62 +1,62 @@
-import { PrismaClient } from '@prisma/client'
+// import { PrismaClient } from '@prisma/client'
 
-// interface Person {
-//   people: {
-//     id: number
-//     Name: string
-//     Date: string
-//     intime: string
-//     outtime: string
-//     Present: string
-//   }
+// // interface Person {
+// //   people: {
+// //     id: number
+// //     Name: string
+// //     Date: string
+// //     intime: string
+// //     outtime: string
+// //     Present: string
+// //   }
+// // }
+
+// const prisma = new PrismaClient()
+
+// // const send_table = (props: Person) => {
+// //   async function main() {
+// //     await prisma.attend.create({
+// //       data: props.people,
+// //     })
+
+// //     const allUsers = await prisma.attend.findMany()
+// //     console.log(allUsers)
+// //   }
+
+// //   main()
+// //     .then(async () => {
+// //       await prisma.$disconnect()
+// //     })
+// //     .catch(async (e) => {
+// //       console.error(e)
+// //       await prisma.$disconnect()
+// //       process.exit(1)
+// //     })
+// // }
+
+// // export default send_table
+
+// async function main() {
+//   await prisma.attend.create({
+//     data: {
+//       id: 462237,
+//       Name: 'Al',
+//       Date: new Date().toLocaleDateString(),
+//       EnterTime: new Date().toLocaleTimeString(),
+//       ExitTime: new Date().toLocaleTimeString(),
+//     },
+//   })
+
+//   const allUsers = await prisma.attend.findMany()
+//   console.log(allUsers)
 // }
 
-const prisma = new PrismaClient()
-
-// const send_table = (props: Person) => {
-//   async function main() {
-//     await prisma.attend.create({
-//       data: props.people,
-//     })
-
-//     const allUsers = await prisma.attend.findMany()
-//     console.log(allUsers)
-//   }
-
-//   main()
-//     .then(async () => {
-//       await prisma.$disconnect()
-//     })
-//     .catch(async (e) => {
-//       console.error(e)
-//       await prisma.$disconnect()
-//       process.exit(1)
-//     })
-// }
-
-// export default send_table
-
-async function main() {
-  await prisma.attend.create({
-    data: {
-      id: 462237,
-      Name: 'Al',
-      Date: new Date().toLocaleDateString(),
-      EnterTime: new Date().toLocaleTimeString(),
-      ExitTime: new Date().toLocaleTimeString(),
-    },
-  })
-
-  const allUsers = await prisma.attend.findMany()
-  console.log(allUsers)
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
-  })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+// main()
+//   .then(async () => {
+//     await prisma.$disconnect()
+//   })
+//   .catch(async (e) => {
+//     console.error(e)
+//     await prisma.$disconnect()
+//     process.exit(1)
+//   })
