4ca82d73bebe4edb15dd3c1ba72099e468793162
kage1020 <4620087@ed.tus.ac.jp>
2023-06-10 20:40:37 +0900

    ✨ スピナーの追加

--- src/app/demo/unity/unity.tsx
+++ src/app/demo/unity/unity.tsx
@@ -1,9 +1,11 @@
 'use client'
 
+import CircularProgress from '@mui/material/CircularProgress'
+import Container from '@mui/material/Container'
 import { Unity, useUnityContext } from 'react-unity-webgl'
 
 const UnityApp = () => {
-  const { unityProvider } = useUnityContext({
+  const { unityProvider, isLoaded } = useUnityContext({
     loaderUrl: '/unity/Build/Build.loader.js',
     dataUrl: '/unity/Build/Build.data',
     frameworkUrl: '/unity/Build/Build.framework.js',
@@ -12,7 +14,25 @@ const UnityApp = () => {
     productName: 'Unitychan Practice',
     productVersion: '0.1',
   })
-  return <Unity unityProvider={unityProvider} className='h-screen w-screen' />
+  return (
+    <>
+      {!isLoaded && (
+        <Container
+          sx={{
+            width: '100lvw',
+            height: '100lvh',
+            display: 'grid',
+            placeItems: 'center',
+            position: 'absolute',
+            inset: 0,
+          }}
+        >
+          <CircularProgress size={60} />
+        </Container>
+      )}
+      <Unity unityProvider={unityProvider} className='h-screen w-screen' />
+    </>
+  )
 }
 
 export default UnityApp
